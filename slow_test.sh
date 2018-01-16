a79acc4c084566ad110c2f61d1a12fa42a55f954
Nicolai Willems <nwi@tradeshift.com>
2018-01-16 23:03:45 +0100

    Adding quick and slow tests

--- slow_test.sh
+++ slow_test.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 echo "Take your time"
+
+git fetch origin
+git checkout stag
+git merge --ff-only origin/testing
+
+# Run the tests - wow much speedup!!!
+sleep 10
+
+git fetch origin
+
+git checkout master
+git rebase origin/master
+
+git merge --log -m "Merge slow-tested changes from testing" --no-ff \
+    $(git rev-list --max-count=1 stag)
+
+# Replace origin/testing below, with the currently tested hash
+# git merge --no-ff origin/testing
